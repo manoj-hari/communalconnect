c1cb652c4fdbb9e65a6deccadc36cbd929baa872
Manoj Kannan <x18131581@student.ncirl.ie>
2019-03-13 09:29:45 +0000

    after down migrating user table from initial vote functionality

--- db/schema.rb
+++ db/schema.rb
@@ -18,13 +18,6 @@ ActiveRecord::Schema.define(version: 2019_03_12_131748) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "vote_values", force: :cascade do |t|
     t.string "title"
     t.integer "poll_id"
