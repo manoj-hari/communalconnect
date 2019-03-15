2ee84970877ac3fbc5537fcb653616624b007c5a
Manoj Kannan <x18131581@student.ncirl.ie>
2019-03-15 11:33:12 +0000

    Polling app with user changes and front end index changes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,14 +1,18 @@
 class VotesController < ApplicationController
     
     def create
+        if current_user && params[:poll] && params[:poll][:id] && params[:vote_value] && params[:vote_value][:id]
         @poll = Poll.find_by_id(params[:poll][:id])
         @option = @poll.vote_values.find_by_id(params[:vote_value][:id])
-         if @option && @poll 
+         if @option && @poll && !current_user.voted_for?(@poll)
                  
-            @option.votes.create
+            @option.votes.create({user_id: current_user.id})
             
          else
              render js: 'alert(\'Your vote cannot be saved. Have you already voted?\');'
          end
+          else
+      render js: 'alert(\'Your vote cannot be saved.\');'
+    end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         has_many :votes, dependent: :destroy
+         has_many :vote_values, through: :votes
+         
+         def voted_for?(poll)
+          vote_values.any? {|v| v.poll == poll }
+         end
 end
